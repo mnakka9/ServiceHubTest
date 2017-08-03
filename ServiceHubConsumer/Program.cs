ad1de5c975e588ea1c4f7a3939d7396e7b5029af
Mahesh Nakka <mnakka9@gmail.com>
2017-08-03 14:31:38 +0530

    Add project files.

---
+++ ServiceHubConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ServiceHubConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var constr = "<<<<>>>>";
+            var client = QueueClient.CreateFromConnectionString(constr);
+
+            client.OnMessage(
+                (message) =>
+                {
+
+                    WriteLine($"Message with {message.MessageId} is processed, and body is {message.GetBody<string>()}");
+
+                }
+                );
+
+            Read();
+        }
+    }
+}
---
+++ ServiceHubCreator/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ServiceHubCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var constr = "<<>>>";
+            var client = QueueClient.CreateFromConnectionString(constr);
+
+            var lastKey = ' ';
+            var count = 1;
+            while(lastKey != 'q')
+            {
+                var message = $"Message{count++}";
+                var bm = new BrokeredMessage(message);
+                client.Send(bm);
+                WriteLine($"Message sent - {bm.MessageId}");
+                lastKey = ReadKey().KeyChar;                
+            }
+        }
+    }
+}
